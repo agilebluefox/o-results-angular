e55bf927b7b39216984b6ed00df2c9282ee46920
David Conner <agilebluefox@gmail.com>
2016-10-29 19:20:15 -0400

    New component to add a new event

---
+++ src/app/events/event-add/event-add.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { EventAddComponent } from './event-add.component';
+
+describe('Component: EventAdd', () => {
+  it('should create an instance', () => {
+    let component = new EventAddComponent();
+    expect(component).toBeTruthy();
+  });
+});
