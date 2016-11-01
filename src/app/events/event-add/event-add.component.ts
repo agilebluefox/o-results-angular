5fbbe54796d35cf32e345b5ba93a1921e3743667
David Conner <agilebluefox@gmail.com>
2016-10-31 22:38:47 -0400

    Add link to cancel action and return to previous page

--- src/app/events/event-add/event-add.component.ts
+++ src/app/events/event-add/event-add.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-event-add',
+  selector: 'event-add',
   templateUrl: './event-add.component.html',
   styleUrls: ['./event-add.component.scss']
 })
 export class EventAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+  }
+
+  goBack(link) {
+    this.location.back();
   }
 
 }
