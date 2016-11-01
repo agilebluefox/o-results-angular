30c58b2adb16d1721128282e9eac2b5404f617f7
David Conner <agilebluefox@gmail.com>
2016-10-31 22:37:56 -0400

    New component to display event information

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+
+import { Event } from '../../events/shared/event';
+import { EVENTS } from '../../shared/mock-events';
 
 @Component({
-  selector: 'app-event-details',
+  selector: 'event-details',
   templateUrl: './event-details.component.html',
   styleUrls: ['./event-details.component.scss']
 })
 export class EventDetailsComponent implements OnInit {
 
-  constructor() { }
+  events = EVENTS;
+  event: Event;
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.forEach((params: Params) => {
+      // Route params are always strings
+      let id = params['id'];
+      console.log(id);
+      console.log(this.events);
+      this.event = this.events.find(event => event.id === id); // move to service
+      // this.heroService.getHero(id)
+      //   .then(hero => this.hero = hero);
+    });
   }
 
 }
