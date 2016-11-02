56235f18619250f7ffbf0d2737c4bb37e5d604d5
agilebluefox <agilebluefox@gmail.com>
2016-11-02 10:15:58 -0400

    Changed date string to ISO format

--- src/app/shared/mock-events.ts
+++ src/app/shared/mock-events.ts
@@ -5,18 +5,18 @@ export const EVENTS: Event[] = [
     id: '1',
     name: '2016 Fall Umstead Park',
     location: 'Umstead Park',
-    date: '2016-12-04'
+    date: '2016-11-21T00:00:00.000Z'
   },
   {
     id: '2',
     name: '2016 Spring Umstead Park',
     location: 'Umstead Park',
-    date: '2016-02-21'
+    date: '2016-02-09T00:00:00.000Z'
   },
   {
     id: '3',
     name: '2015 Fall Umstead Park',
     location: 'Umstead Park',
-    date: '2015-12-05'
+    date: '2015-12-05T00:00:00.000Z'
   }
 ];
