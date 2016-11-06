23aba8bff6bbfb54bd5050bc9a47b2614fb7d8e3
David Conner <agilebluefox@gmail.com>
2016-11-06 17:10:57 -0500

    Add component to add controls

---
+++ src/app/controls/control-add/control-add.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-control-add',
+  templateUrl: './control-add.component.html',
+  styleUrls: ['./control-add.component.scss']
+})
+export class ControlAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addControlToEvent() {
+    
+  }
+
+}
