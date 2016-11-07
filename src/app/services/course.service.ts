5a44dd40b0be1d4e1a7a6d11dbdbe22ac37e2482
David Conner <agilebluefox@gmail.com>
2016-11-07 14:32:10 -0500

    Add service to provide course data
    This will decouple the modules that rely on the data with the way the data is stored or accessed.

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 
+<<<<<<< HEAD
 import { Course } from '../courses/shared/course';
 import { COURSES } from '../shared/mock-courses';
 
@@ -12,6 +13,18 @@ export class CourseService {
 
   getCourses(): Promise<Course[]> {
     return Promise.resolve(COURSES);
+=======
+import { Class } from '../classes/shared/class';
+import { CLASSES } from '../shared/mock-classes';
+
+@Injectable()
+export class CourseService {
+
+  constructor() { }
+
+  getClasses(): Promise<Class[]> {
+    return Promise.resolve(CLASSES);
+>>>>>>> e0f61f4... Add service to provide course data
   }
 
 }
