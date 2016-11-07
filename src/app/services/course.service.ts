1bae43f728f57829f49ec9118d8b87c956863de8
David Conner <agilebluefox@gmail.com>
2016-11-07 14:58:43 -0500

    Setup services for data models
    Each service starts with a single method that returns a Promise in the form of an array containing the data related to the model.

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Course } from '../courses/shared/course';
 import { COURSES } from '../shared/mock-courses';
 
@@ -16,15 +17,26 @@ export class CourseService {
 =======
 import { Class } from '../classes/shared/class';
 import { CLASSES } from '../shared/mock-classes';
+=======
+import { Course } from '../courses/shared/course';
+import { COURSES } from '../shared/mock-courses';
+>>>>>>> 838813c... Setup services for data models
 
 @Injectable()
 export class CourseService {
+  courses: Course[] = [];
+  course: Course;
 
   constructor() { }
 
+<<<<<<< HEAD
   getClasses(): Promise<Class[]> {
     return Promise.resolve(CLASSES);
 >>>>>>> e0f61f4... Add service to provide course data
+=======
+  getCourses(): Promise<Course[]> {
+    return Promise.resolve(COURSES);
+>>>>>>> 838813c... Setup services for data models
   }
 
 }
