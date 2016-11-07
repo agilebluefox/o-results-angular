688b964cb2b19b6a1ad7833de74335b403e0e9a5
David Conner <agilebluefox@gmail.com>
2016-11-07 16:19:47 -0500

    Merge changes in services

--- src/app/services/class.service.ts
+++ src/app/services/class.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 
-<<<<<<< HEAD
 import { Class } from '../classes/shared/class';
 import { CLASSES } from '../shared/mock-classes';
 
@@ -14,12 +13,5 @@ export class ClassService {
   getClasses(): Promise<Class[]> {
     return Promise.resolve(CLASSES);
   }
-
-=======
-@Injectable()
-export class ClassService {
-
-  constructor() { }
-
->>>>>>> 606b34f... Add a class service
+  
 }
