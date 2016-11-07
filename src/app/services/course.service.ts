abf6a954bef0498fbd601417caf3476e0e0c8ae8
David Conner <agilebluefox@gmail.com>
2016-11-07 16:19:47 -0500

    Merge changes in services

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,55 +1,16 @@
 import { Injectable } from '@angular/core';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Course } from '../courses/shared/course';
 import { COURSES } from '../shared/mock-courses';
 
 @Injectable()
 export class CourseService {
-  courses: Course[] = [];
-  course: Course;
+    courses: Course[] = [];
+    course: Course;
 
-  constructor() { }
+    constructor() { }
 
-  getCourses(): Promise<Course[]> {
-    return Promise.resolve(COURSES);
-=======
-import { Class } from '../classes/shared/class';
-import { CLASSES } from '../shared/mock-classes';
-=======
-import { Course } from '../courses/shared/course';
-import { COURSES } from '../shared/mock-courses';
->>>>>>> 838813c... Setup services for data models
-
-@Injectable()
-export class CourseService {
-  courses: Course[] = [];
-  course: Course;
-
-  constructor() { }
-
-<<<<<<< HEAD
-  getClasses(): Promise<Class[]> {
-    return Promise.resolve(CLASSES);
->>>>>>> e0f61f4... Add service to provide course data
-=======
-  getCourses(): Promise<Course[]> {
-    return Promise.resolve(COURSES);
->>>>>>> 838813c... Setup services for data models
-=======
-import { Class } from '../classes/shared/class';
-import { CLASSES } from '../shared/mock-classes';
-
-@Injectable()
-export class CourseService {
-
-  constructor() { }
-
-  getClasses(): Promise<Class[]> {
-    return Promise.resolve(CLASSES);
->>>>>>> e0f61f4... Add service to provide course data
-  }
-
+    getCourses(): Promise<Course[]> {
+        return Promise.resolve(COURSES);
+    }
 }
