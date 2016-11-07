fe37d9ba0d9b6b5bdf283af685260b9ce266e220
David Conner <agilebluefox@gmail.com>
2016-11-07 13:38:08 -0500

    Add the _id property I want to make the mock data match the actual data as much as possible. The mongo _id field will be used extensively to store and track access to the individual documents.

--- src/app/courses/shared/course.ts
+++ src/app/courses/shared/course.ts
@@ -1,6 +1,7 @@
 import { Control } from '../../controls/shared/control';
 
 export class Course {
+    _id: string;
     location: string;
     name: string;
     mapdate: string;
--- src/app/shared/mock-classes.ts
+++ src/app/shared/mock-classes.ts
@@ -2,6 +2,7 @@ import { Class } from '../classes/shared/class';
 
 export const CLASSES: Class[] = [
     {
+        "_id": "57fc2a422e75cf056c89902e",
         "year": 2016,
         "semester": "Fall",
         "prefix": "HESO",
@@ -10,6 +11,7 @@ export const CLASSES: Class[] = [
         "section": "194"
     },
     {
+        "_id": "57fc2a422e75cf056c89902f",
         "year": 2016,
         "semester": "Spring",
         "prefix": "HESO",
@@ -18,6 +20,7 @@ export const CLASSES: Class[] = [
         "section": "196"
     },
     {
+        "_id": "57fc2a422e75cf056c899030",
         "year": 2015,
         "semester": "Fall",
         "prefix": "HESO",
@@ -26,6 +29,7 @@ export const CLASSES: Class[] = [
         "section": "194"
     },
     {
+        "_id" : "57fc2a422e75cf056c899031",
         "year": 2015,
         "semester": "Spring",
         "prefix": "HESO",
