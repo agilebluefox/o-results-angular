ba060f371924f617a5cf3c9eaf315111af9ad7e7
David Conner <agilebluefox@gmail.com>
2016-11-11 20:15:05 -0500

    Add http access to mock event data

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,36 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Event } from '../events/shared/event';
-import { EVENTS } from '../shared/mock-events';
 
 @Injectable()
 export class EventService {
-  events: Event[] = [];
-  event: Event;
 
-  constructor() { }
+  // The url will be a JSON file for now
+  private eventsUrl = 'app/shared/mock-events.json';
 
-/**
- * Get events.
- * 
- * @returns {Promise<Event[]>}
- * 
- * @memberOf EventService
- */
-  getEvents(): Promise<Event[]> {
-    return Promise.resolve(EVENTS);
+  // Setup http headers
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+
+  // Handle error if the http request fails
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
   }
 
-/**
- * 
- * 
- * @param {String} id
- * @returns {Promise<Event>}
- * 
- * @memberOf EventService
- */
+  constructor(private http: Http) { }
+
+  // Get the events and return a promise
+  getEvents(): Promise<Event[]> {
+    return this.http.get(this.eventsUrl)
+      .toPromise()
+      .then(response => response.json().data as Event[])
+      .catch(this.handleError);
+  }
+
+  // Method to get an event by id
   getEvent(id: string): Promise<Event> {
-    return this.getEvents().then(events => events.find(event => event.id === id));
+    return this.getEvents().then(events => events.find(event => event._id === id));
   }
 
 }
