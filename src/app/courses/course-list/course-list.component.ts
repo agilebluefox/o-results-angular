52e23213d9bd98eb9758a9a3fef33ca7fb356c4a
David Conner <agilebluefox@gmail.com>
2016-11-13 18:58:16 -0500

    Show the list of the mock course data
    Add the methods to get and render the list of courses from the mock data server, the JSON file.

--- src/app/courses/course-list/course-list.component.ts
+++ src/app/courses/course-list/course-list.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import { Course } from '../shared/course';
+import { CourseService } from '../../services/course.service';
+
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent implements OnInit {
+  courses: Course[] = [];
+  course: Course;
 
-  constructor() { }
+  constructor(
+    private courseService: CourseService,
+    private router: Router
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.courseService.getCourses()
+      .then(courses => this.courses = courses);
+  }
+
+  getCourses(): void {
+    this.courseService.getCourses()
+      .then(courses => this.courses = courses);
+  }
+
+  goToDetails(course: Course): void {
+    let link = ['/course-details', course._id];
+    this.router.navigate(link);
+  }
+
+  addNewCourse(): void {
+    this.router.navigate(['/course-add']);
   }
 
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,41 +12,32 @@ import { EventService } from '../services/event.service';
 
 export class DashboardComponent implements OnInit {
 
-  events: Event[] = [];
+  // Public property to expose events array
+  events: Event[];
 
-/**
- * Creates an instance of DashboardComponent.
- * 
- * @param {Router} router
- * @param {EventService} EventService
- * 
- * @memberOf DashboardComponent
- */
+  // Inject the services
   constructor(
     private router: Router,
     private eventService: EventService
   ) { }
 
+  // Call getEvents to add the list when the page loads
   ngOnInit(): void {
-    this.eventService.getEvents()
-      .then(events => this.events = events.slice(0,3));
+    this.getEvents();
   }
 
-  /**
-   * Get the events.
-   */
+  // Get the events
   getEvents(): void {
     // Resolves a promise from the EventService
-    this.eventService.getEvents().then(events => this.events = events);
+    this.eventService.getEvents().then(events => { console.log(events); this.events = events });
   }
 
   goToDetails(event: Event): void {
-    let link = ['/event-dashboard', event.id];
+    let link = ['/event-dashboard', event._id];
     this.router.navigate(link);
   }
-/**
- * Add a new event
- */
+
+  // Add an event
   addNewEvent(): void {
     // Navigate to the add form component
     this.router.navigate(['/event-add']);
