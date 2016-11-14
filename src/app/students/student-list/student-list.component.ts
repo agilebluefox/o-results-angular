e805a90cb256a03fded5d20d8f1912dc81b5738e
David Conner <agilebluefox@gmail.com>
2016-11-13 21:53:48 -0500

    Get the list of students
    Inject the student service and get the list of students from the mock data JSON file.

--- src/app/students/student-list/student-list.component.ts
+++ src/app/students/student-list/student-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Student } from '../shared/student';
+import { StudentService } from '../../services/student.service';
 
 @Component({
   selector: 'app-student-list',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentListComponent implements OnInit {
 
-  constructor() { }
+  students: Student[] = [];
+  student: Student;
 
-  ngOnInit() {
+  constructor(
+    private studentService: StudentService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.studentService.getStudents()
+      .then(students => this.students = students);
   }
 
+  getStudents(): void {
+    this.studentService.getStudents()
+      .then(students => this.students = students);
+  }
+
+  goToDetails(student: Student): void {
+    let link = ['/student-details', student._id];
+    this.router.navigate(link);
+  }
+
+  addNewStudent(): void {
+    this.router.navigate(['/student-add']);
+  }
 }
