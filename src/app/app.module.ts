eaa50f6f2103b6e534ebf55cb1d6489446f1cca1
David Conner <agilebluefox@gmail.com>
2016-11-15 09:14:31 -0500

    Create data-driven form to add events
    The data driven form is one of the options when crreating forms in Angular2. The advantage is the form is accessible in the component without binding. Using the Validators directives handles validation in the component rather than the template.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '@angular/material';
@@ -44,7 +44,8 @@ import { ClassListComponent } from './classes/class-list/class-list.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
     EventService,
--- src/app/events/event-add/event-add.component.ts
+++ src/app/events/event-add/event-add.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'event-add',
   templateUrl: './event-add.component.html',
@@ -9,12 +11,23 @@ import { Location } from '@angular/common';
 })
 export class EventAddComponent implements OnInit {
 
+  eventAddForm: FormGroup;
+
   constructor(
     private router: Router,
     private location: Location
   ) { }
 
   ngOnInit() {
+    this.eventAddForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      location: new FormControl(null, Validators.required),
+      date: new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.eventAddForm);
   }
 
   goBack(link) {
