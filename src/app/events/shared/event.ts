b97bcf9083cecc93f4379513c03ea9ebc61899fd
David Conner <agilebluefox@gmail.com>
2016-11-15 09:25:08 -0500

    Add optional indicators to event model properties
    To start off attempting to post data to the server, I want to make the properties that accept MongoId's in the form of arrays optional. This may not be a bad idea since the event will be built over time and not all at once. Therefore, some data, such as the list of courses, won't be available when the basic properties of the event are added. I can update the event later with the missing data.

--- src/app/events/shared/event.ts
+++ src/app/events/shared/event.ts
@@ -2,11 +2,11 @@ import { Class } from '../../classes/shared/class';
 import { Course } from '../../courses/shared/course';
 
 export class Event {
-  _id: string;
+  _id?: string;
   name: string;
   location: string;
   date: string;
-  classes: Class[];
-  courses: Course[];
-  active: boolean;
+  classes?: Class[];
+  courses?: Course[];
+  active?: boolean;
 }
