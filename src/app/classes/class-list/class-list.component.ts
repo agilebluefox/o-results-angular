6c87f3873835fd4f4eeec860ccac8c578d7e8126
David Conner <agilebluefox@gmail.com>
2016-11-19 21:06:36 -0500

    Select classes for event
    This is a demo feature that illustrates selecting a class and adding it to the list of classes that will be part of the event. No data is stored and the process is controlled by moving the classes from one array to another.

--- src/app/classes/class-list/class-list.component.ts
+++ src/app/classes/class-list/class-list.component.ts
@@ -12,6 +12,8 @@ import { ClassService } from '../../services/class.service';
 export class ClassListComponent implements OnInit {
   classes: Cls[] = [];
   cls: Cls;
+  selectedClass: Cls;
+  classesInEvent: Cls[] = [];
 
   constructor(
     private classService: ClassService,
@@ -35,6 +37,16 @@ export class ClassListComponent implements OnInit {
 
   addNewClass(): void {
     this.router.navigate(['/class-add']);
+  }
+
+  addToEvent(cls: Cls): void {
+    this.classesInEvent.push(cls);
+    this.classes.splice(this.classes.indexOf(cls),1);
+  }
+
+  removeFromEvent(cls: Cls): void {
+    this.classesInEvent.splice(this.classesInEvent.indexOf(cls),1);
+    this.classes.push(cls);
   }
 
 }
