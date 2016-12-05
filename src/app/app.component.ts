e1fc3fd211ebe451f9ac4f0ccb3d10f8680ee8bd
David Conner <agilebluefox@gmail.com>
2016-12-04 19:34:02 -0500

    Use the app component to initiate the event call
    I'm searching for a means of retrieving the events array as early as possible.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+
+import { EventService } from './services/event.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
-
-
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'oResults';
+  @Output() events: Event[];
 
+  constructor(private eventService: EventService) {}
+
+  ngOnInit() {
+    let populate = false;
+    let active = true;
+    this.eventService.getEvents((events) => { this.events = events }, populate, active);
+  }
 
 }
