41e225eacc5ceeb58d5f16d5d62ae405eb530b2d
David Conner <agilebluefox@gmail.com>
2017-03-28 20:15:00 -0400

    Replace generic Rx import statement

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { Headers, Http, Response } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
-import 'rxjs/Rx';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 import { Student } from '../models/student.model';
 
